7ce41a0f16fd210288a43d84e285cac171e366f3
PedroCostaa <66277802+PedroCostaa@users.noreply.github.com>
2020-06-01 19:14:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function go_home(){
-    window.location= "file:///C:/Users/Marcela/Documents/GitHub/Pagina-Pessoal/index.html";
+    window.location= "index.html";
 }
 
 function go_social(){
-    window.location = "file:///C:/Users/Marcela/Documents/GitHub/Pagina-Pessoal/socialMedia.html";
+    window.location = "socialMedia.html";
 }
 
 function go_curriculum(){
-    window.location = "file:///C:/Users/Marcela/Documents/GitHub/Pagina-Pessoal/curriculum.html";
+    window.location = "curriculum.html";
 }
