755113d73e58afd1ec1fa9019d4ba66f449b831d
PedroCostaa <66277802+PedroCostaa@users.noreply.github.com>
2020-06-01 18:54:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function go_home(){
+    window.location= "file:///C:/Users/Marcela/Documents/GitHub/Pagina-Pessoal/index.html";
+}
+
+function go_social(){
+    window.location = "file:///C:/Users/Marcela/Documents/GitHub/Pagina-Pessoal/socialMedia.html";
+}
+
+function go_curriculum(){
+    window.location = "file:///C:/Users/Marcela/Documents/GitHub/Pagina-Pessoal/curriculum.html";
+}
